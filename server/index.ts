8c58ea04d2cabe40124d63383849dc64eb94cba9
Dan1el71 <ddrmhk1@gmail.com>
2025-03-12 18:34:35 -0500

    Refactor server initialization to include Redis connection

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,8 @@
 import app from './app'
-import { PORT } from './config'
+import { PORT } from './config/config'
+import { connectRedis } from './config/redis'
+
+connectRedis()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
