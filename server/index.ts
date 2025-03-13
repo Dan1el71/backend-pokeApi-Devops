9d5775e98c0f22fa76883759eb8e6e8c14d361c5
Dan1el71 <ddrmhk1@gmail.com>
2025-03-12 22:30:10 -0500

    Comment out Redis connection implementation in server index file

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,8 @@ import app from './app'
 import { PORT } from './config/server'
 import { connectRedis } from './config/redis'
 
-connectRedis()
+//Pendiente por implementar
+//connectRedis()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
