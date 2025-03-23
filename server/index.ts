edb477236322ac124c06c90d3b7b6e3202a47894
Dan1el71 <ddrmhk1@gmail.com>
2025-03-23 01:34:20 -0500

    Remove Redis configuration and cache service implementation

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,5 @@
 import app from './app'
 import { PORT } from './config/server'
-import { connectRedis } from './config/redis'
-
-//connectRedis()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
